07182e3a54bcefcaf775c73fde886c0bbc9dde5a
webler-biology-physiology <csabavadasz79@gmail.com>
2023-11-18 11:53:31 +0100

    code readability

--- src/main/java/hu/webler/repository/DatabaseRepository.java
+++ src/main/java/hu/webler/repository/DatabaseRepository.java
@@ -5,7 +5,11 @@ import java.sql.Connection;
 public class DatabaseRepository {
 
     private static Connection connection;
-    private static final String JDBC_URL = "jdbc:mysql://localhost:" + System.getenv("DB_PORT") + "/" + System.getenv("DB_URL") + "?createDatabaseIfNotExist=true&useSSL=true&sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:"
+            + System.getenv("DB_PORT")
+            + "/"
+            + System.getenv("DB_URL")
+            + "?createDatabaseIfNotExist=true&useSSL=true&sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false";
     private static final String DB_USER = System.getenv("DB_USER");
     private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 }
